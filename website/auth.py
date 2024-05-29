bce02d81cf2d714c81dc86ee7492fac4db1b6d3d
enjanurr <bajentingjanurr@gmail.com>
2024-05-29 14:31:36 +0800

    anader wan

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=["POST","GET"])
 def login():
-    return render_template('question.html', text= 'hello')
+    data = request.form
+    print(data)
+    return render_template('index.html', boolean=True)
 
-@auth.route('/questions')
+@auth.route('/questions', methods=["POST","GET"])
 def logout():
-    return render_template('question.html', text= 'hiho')
+    return render_template('question.html')
 
 @auth.route('/sign-up')
 def sign_up():
